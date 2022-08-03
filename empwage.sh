a8aa39089a5281d091951134ebb78e1eb34fa572
iamsaqlain_q <iamkhatik786@gmail.com>
2022-08-03 16:14:35 +0530

    Add part time employee & wage

--- empwage.sh
+++ empwage.sh
@@ -12,3 +12,17 @@ empRatePerHr=20;
 empHrs=8;
 salary=$(($empHrs*$empRatePerHr));
 echo "$salary" 
+
+isPartTime=1; 
+isFullTime=2; 
+empRatePerHr=20;
+randomCheck=$((RANDOM%3));
+if [ $isFullTime -eq $randomCheck ]
+then
+	empHrs=8;
+elif [ $isPartTime -eq $randomCheck ]
+	empHrs=4;
+else
+	empHrs=0;
+fi
+salary=$(($empHrs*$empRatePerHr));
